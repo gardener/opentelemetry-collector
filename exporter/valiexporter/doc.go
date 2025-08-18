3561a90536c46d3100ab08b72a02b56c664e5bd1
Radoslav Hubenov <rrhubenov@gmail.com>
2025-08-18 11:44:36 +0300

    Remove valiexporter component

--- exporter/valiexporter/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright SAP SE or an SAP affiliate company and Gardener contributors
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate mdatagen metadata.yaml
-
-package valiexporter // import "github.com/gardener/opentelemetry-collector/exporter/valiexporter"

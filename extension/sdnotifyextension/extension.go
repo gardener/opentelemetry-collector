8b985bdf84235558fec55942938f2dc167f24128
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-06-30 10:17:27 +0300

    feat: add minimal setup so the extension to be systemd aware
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- extension/sdnotifyextension/extension.go
+++ extension/sdnotifyextension/extension.go
@@ -2,7 +2,10 @@ package sdnotifyextension
 
 import (
 	"context"
+	"fmt"
+	"os"
 
+	"github.com/coreos/go-systemd/v22/daemon"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/extension"
 	"go.opentelemetry.io/collector/extension/extensioncapabilities"
@@ -33,6 +36,12 @@ func newSDNotify(cfg *Config, logger *zap.Logger) *sdnotify {
 
 func (s *sdnotify) Start(_ context.Context, host component.Host) error {
 	s.host = host
+
+	// If the variable is not set, the protocol is a no-op.
+	if s.cfg.FailIfNotSupervised && os.Getenv("NOTIFY_SOCKET") == "" {
+		return fmt.Errorf("sdnotify: NOTIFY_SOCKET not set; not running under systemd")
+	}
+
 	return nil
 }
 
@@ -41,9 +50,31 @@ func (s *sdnotify) Shutdown(_ context.Context) error {
 }
 
 func (s *sdnotify) Ready() error {
+	sent, err := daemon.SdNotify(false, daemon.SdNotifyReady)
+	if err != nil {
+		return fmt.Errorf("sdnotify READY=1: %w", err)
+	}
+
+	if !sent {
+		s.logger.Info("sdnotify: NOTIFY_SOCKET not set; READY=1 was a no-op")
+	} else {
+		s.logger.Info("sdnotify: sent READY=1 to systemd")
+	}
+
 	return nil
 }
 
 func (s *sdnotify) NotReady() error {
+	sent, err := daemon.SdNotify(false, daemon.SdNotifyStopping)
+	if err != nil {
+		// Don't block shutdown on a notify failure - just log it.
+		s.logger.Warn("sdnotify STOPPING=1 failed", zap.Error(err))
+		return nil
+	}
+
+	if sent {
+		s.logger.Info("sdnotify: sent STOPPING=1 to systemd")
+	}
+
 	return nil
 }

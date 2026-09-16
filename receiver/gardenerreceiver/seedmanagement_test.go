e7f3e82fa12d6ad3cb7cb285f70b86c581a2c3fc
Christoph Kleineweber <c.kleineweber@sap.com>
2026-09-16 10:41:27 +0200

    Refactor condition metrics (#73)
    * Refactor condition metrics
    
    Move the condition status from an attribute to metric value to reduce
    cardinality. The reason attribute is removed also for cardinality
    reasons.
    
    Signed-off-by: Christoph Kleineweber <c.kleineweber@sap.com>
    
    * Cleanup tests
    
    Signed-off-by: Christoph Kleineweber <c.kleineweber@sap.com>
    
    ---------
    
    Signed-off-by: Christoph Kleineweber <c.kleineweber@sap.com>

--- receiver/gardenerreceiver/seedmanagement_test.go
+++ receiver/gardenerreceiver/seedmanagement_test.go
@@ -134,9 +134,11 @@ func TestCollectGardenletMetrics(t *testing.T) {
 			require.Equal(t, int64(4), m.Gauge().DataPoints().At(0).IntValue())
 		case "garden.gardenlet.condition":
 			require.Equal(t, 1, m.Gauge().DataPoints().Len())
-			condType, ok := m.Gauge().DataPoints().At(0).Attributes().Get("gardener.condition.type")
+			dp := m.Gauge().DataPoints().At(0)
+			condType, ok := dp.Attributes().Get("gardener.condition.type")
 			require.True(t, ok)
 			require.Equal(t, "GardenletReconciled", condType.Str())
+			require.Equal(t, int64(1), dp.IntValue(), "expected value 1 for ConditionTrue status")
 		}
 	}
 }
